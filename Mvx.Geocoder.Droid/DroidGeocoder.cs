8ced82597a2d4ced8e6741ee82f2626b50330186
Seifer <seed-seifer@ya.ru>
2014-06-12 17:02:11 +0300

    Added Droid Geocoder

---
+++ Mvx.Geocoder.Droid/DroidGeocoder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Cirrious.CrossCore;
+using System.Linq;
+
+namespace MvxPlugins.Geocoder.Droid
+{
+	public class DroidGeocoder: IGeocoder
+	{
+		public async Task<Address[]> GetAddressesAsync (double latitude, double longitude)
+		{
+			var globals = Mvx.Resolve<Cirrious.CrossCore.Droid.IMvxAndroidGlobals>();
+			var geocoder = new Android.Locations.Geocoder (globals.ApplicationContext);
+
+			var addresses = await geocoder.GetFromLocationAsync (latitude, longitude, 10);
+
+			return addresses.Select (Convert).ToArray ();
+		}
+
+		private static Address Convert (Android.Locations.Address address)
+		{
+			return new Address () {
+				Latitude = address.Latitude,
+				Longitude = address.Longitude,
+				Name = address.FeatureName,
+				Country = address.CountryName,
+				PostalCode = address.PostalCode,
+			};
+		}
+	}
+}
+
---
+++ Mvx.Geocoder.Droid/Plugin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Cirrious.CrossCore.Plugins;
+using Cirrious.CrossCore;
+
+namespace MvxPlugins.Geocoder.Droid
+{
+	public class Plugin
+		: IMvxPlugin
+	{
+		public void Load()
+		{
+			Mvx.RegisterSingleton<IGeocoder>(new DroidGeocoder ());
+		}
+	}
+}
