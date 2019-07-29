ef9c262cadc8422a8cd7d5a53cc1528a15cfcb5b
Seifer <alexey.shikov@gmail.com>
2019-07-29 13:48:25 +0300

    [iOS] Use new Mvx approach to initialize App

--- Mvx.Geocoder.Sample.Touch/AppDelegate.cs
+++ Mvx.Geocoder.Sample.Touch/AppDelegate.cs
@@ -1,6 +1,7 @@
 ﻿using Foundation;
 using MvvmCross.Platforms.Ios.Core;
 using MvvmCross.ViewModels;
+using Mvx.Geocoder.Sample.Core;
 using UIKit;
 
 namespace Mvx.Geocoder.Sample.Touch
@@ -9,34 +10,8 @@ namespace Mvx.Geocoder.Sample.Touch
 	// User Interface of the application, as well as listening (and optionally responding) to
 	// application events from iOS.
 	[Register ("AppDelegate")]
-	public partial class AppDelegate : MvxApplicationDelegate
+	public class AppDelegate : MvxApplicationDelegate<MvxIosSetup<App>, App>
 	{
-		// class-level declarations
-		private UIWindow _window;
-
-		//
-		// This method is invoked when the application has loaded and is ready to run. In this
-		// method you should instantiate the window, load the UI into it and then make the window
-		// visible.
-		//
-		// You have 17 seconds to return from this method, or iOS will terminate your application.
-		//
-		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
-		{
-			// create a new window instance based on the screen size
-			_window = new UIWindow (UIScreen.MainScreen.Bounds);
-			
-			var setup = new Setup();
-			setup.PlatformInitialize(this, _window);
-
-			var startup = MvvmCross.Mvx.IoCProvider.Resolve<IMvxAppStart>();
-			startup.Start();
-
-			// make the window visible
-			_window.MakeKeyAndVisible ();
-			
-			return true;
-		}
 	}
 }
 
